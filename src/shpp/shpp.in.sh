8f5608caee9839c00ec7082e321b2143b4a99501
Björn Bidar <theodorstormgrade@gmail.com>
2013-04-20 20:25:10 +0200

    fixed color detection

--- src/shpp/shpp.in.sh
+++ src/shpp/shpp.in.sh
@@ -735,7 +735,7 @@ if [ ! $# = 0 ] ; then
 		    [ ! $FORCE_COLOR = n ] ) ; then
 		    # use only colored out if enabled and 
 		    # if output goes to the terminal
-		    if  [ $USE_COLOR ] && [ ! $USE_COLOR = [Nn] ] || \
+		    if  [ $USE_COLOR ] && [ ! $USE_COLOR = n ] || \
 			( [ $FORCE_COLOR ] && [ ! $FORCE_COLOR = n ]  ) ; then 
 			
  			if tput setaf 0 > /dev/null 2>&1 ; then
