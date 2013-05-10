9b83b853153a7c40f1679c68912696e5c9d87b28
Björn Bidar <theodorstormgrade@gmail.com>
2013-05-10 22:36:34 +0200

    fixed ifs issues

--- external/libsh/src/shload.in.sh
+++ external/libsh/src/shload.in.sh
@@ -16,10 +16,10 @@ shload()
 	__shl_error_status=$?
 	;;
       *)
-	    old_ifs=$IFS
+	    LIBSH_shload_old_ifs=$IFS
 	    IFS=:
 	    for __lib_dir in ${IMPORT_LIBRARY_PATH}:${SH_LIBRARY_PATH}; do
-		old_ifs=$IFS
+		IFS=$LIBSH_shload_old_ifs
 		if [ -e $__lib_dir/$1 ] && [ -f $__lib_dir/$1 ] ; then 
 		    . ${__lib_dir}/$1
 		    __shl_error_status=$?
@@ -27,9 +27,10 @@ shload()
 		fi
 		IFS=:
 	    done
+	    IFS=$LIBSH_shload_old_ifs
 	    ;;
     esac
-    unset  __lib __lib_dir
+    unset  __lib __lib_dir LIBSH_shload_old_ifs
     return $__shl_error_status
 }
 
@@ -37,17 +38,17 @@ import()
 # . file with check if already . it
 {
     while [ ! $# = 0 ] ; do
-	old_ifs=$IFS
+	LIBSH_import_old_ifs=$IFS
 	IFS=:
 	for __lib in $LIBSH_IMPORTED ; do
-	    IFS=$old_ifs
+	    IFS=$LIBSH_import_old_ifs
 	    if [ "$__lib" = $1 ] ; then
 		__lib_aready_imported=true
 		break 
 	    fi
 	    IFS=:
 	done 
-	IFS=$old_ifs
+	IFS=$LIBSH_import_old_ifs
 	if [   -z $__lib_aready_imported  ] ; then
 	    if shload $1 ;then
 		LIBSH_IMPORTED=$LIBSH_IMPORTED:$1
@@ -57,7 +58,7 @@ import()
 	    fi
 	fi
 	shift
-	unset __lib_aready_imported
+	unset __lib_aready_imported LIBSH_import_old_ifs
     done  
     return 0 # return how many libs were already imported
 }
