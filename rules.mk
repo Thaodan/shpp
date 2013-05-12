6f22a2efaf2a3e1cfcd17354ad386e275e57f7be
Björn Bidar <theodorstormgrade@gmail.com>
2013-05-12 20:56:35 +0200

    move general rules to rules.mk

---
+++ rules.mk
@@ -0,0 +1,10 @@
+SHPP	      = shpp
+DESTDIR       =
+PREFIX        = /usr/local 
+INSTALL       = /usr/bin/install -D
+MSGFMT        = /usr/bin/msgfmt
+SED           = /bin/sed
+bindir        = $(PREFIX)/bin
+libdir        = $(PREFIX)/lib
+sysconfdir    = $(PREFIX)/etc
+
