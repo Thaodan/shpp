07e93a5eecbc79d0d412811f5f7653ac0fba1212
Björn Bidar <theodorstormgrade@gmail.com>
2013-08-21 21:18:28 +0200

    use LIBSH as naming sheme

--- external/libsh/src/test_input.in.sh
+++ external/libsh/src/test_input.in.sh
@@ -1,12 +1,12 @@
 test_input () { 
-  DMSG_test_input_N=$(( $# + 1 ))
-  DMSG_test_input_errmsg="$( read_farray "$err_input_messages" $DMSG_test_input_N)"
+  LIBSH_test_input_N=$(( $# + 1 ))
+  LIBSH__test_input_errmsg="$( read_farray "$err_input_messages" $DMSG_test_input_N)"
   if [ -n "$DMSG_test_input_errmsg" ] ; then
       d_msg ! 'wrong input' "$DMSG_test_input_errmsg"
       if [   $# = 0   ]; then
 	return 1
       else
-	  return 
+	  return $LIBSH_test_input_N
       fi
   fi
 }
