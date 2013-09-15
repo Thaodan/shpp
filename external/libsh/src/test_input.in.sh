5d4b1e34f46e2330c32bd9a1ed88a82d90222901
Björn Bidar <theodorstormgrade@gmail.com>
2013-09-15 21:00:58 +0200

    this was missing

--- external/libsh/src/test_input.in.sh
+++ external/libsh/src/test_input.in.sh
@@ -1,6 +1,6 @@
 test_input () { 
   LIBSH_test_input_N=$(( $# + 1 ))
-  LIBSH__test_input_errmsg="$( read_farray "$err_input_messages" $DMSG_test_input_N)"
+  LIBSH__test_input_errmsg="$( read_farray "$err_input_messages" $LIBSH_test_input_N)"
   if [ -n "$DMSG_test_input_errmsg" ] ; then
       d_msg ! 'wrong input' "$DMSG_test_input_errmsg"
       if [   $# = 0   ]; then
