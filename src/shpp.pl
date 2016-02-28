d808f27e608fea9f2dd796255d59d181af1d41da
Björn Bidar <theodorstormgrade@gmail.com>
2016-02-28 21:22:41 +0100

    final merge reverted issued commit

--- src/shpp.pl
+++ src/shpp.pl
@@ -459,7 +459,6 @@ sub ifndef {
 $subs->add( 'ifndef', 'ifndef', 'ALL' );
 
 sub Else() {
-   
     # else token
     if ( $l_cmd_ret == 1 ) {
 
@@ -477,8 +476,6 @@ sub Else() {
 $subs->add( 'else', 'Else', 0 );
 
 sub end() {
-   print $l_cmd_ret;
-   print @end;
     # if was fine we end this
     if ( $l_cmd_ret == 1 ) {
         $end[-1] = $$command{line};
