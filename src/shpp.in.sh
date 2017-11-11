bf67ef2a86aa249ee4df241e5517089c3d60f149
Björn Bidar <theodorstormgrade@gmail.com>
2017-11-11 02:47:28 +0100

    remove spaces after line break

--- src/shpp.in.sh
+++ src/shpp.in.sh
@@ -587,8 +587,8 @@ call a new instance ${parser+of} ${parser}to process file"
     case ${__parser:-SELF} in  
 	shpp)  $shpp   --tmp $tmp_dir/slaves --stdout \
 	    "$__cleaned_include"> \
-	    $tmp_dir/$IID/include/files/\ 
-	    ${current_include_no}${__outputfile__cleaned_include}  || \ 
+	    $tmp_dir/$IID/include/files/\
+	    ${current_include_no}${__outputfile__cleaned_include}  || \
 	     error "spawned copy of ourself: $appname returned $?, quiting" ;; 
 	take)
 	    mv $__cleaned_include $tmp_dir/$IID/include/files/${current_include_no}${__outputfile__cleaned_include}
