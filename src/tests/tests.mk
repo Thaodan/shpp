8082ebcb7d96c61339c99d1b848d37dd7a034b9a
Björn Bidar <theodorstormgrade@gmail.com>
2019-02-15 03:35:23 +0100

    tests: don't output to $RESULT when not defined

--- src/tests/tests.mk
+++ src/tests/tests.mk
@@ -2,15 +2,17 @@ TESTFILES	        =  $(wildcard *.sh)
 TARGET		        =  ./../shpp
 TARGET_SHELL		=  
 override TARGET_ARGS    +=  --stdout --errexit
-RESULT 			=
+RESULT 			= 0
+ifneq ($(RESULT),0)
+	RESULT_STR=> $(RESULT)/$(@) 2>&1
+endif
 
 all: $(TESTFILES)
 
 $(RESULT):
 	mkdir $(RESULT)
 $(TESTFILES): $(TARGET) $(RESULT)
-	$(TARGET_SHELL) $(TARGET) $(TARGET_ARGS) $(@)  > \
-	$(RESULT)/$(@) 2>&1
+	$(TARGET_SHELL) $(TARGET) $(TARGET_ARGS) $(@) $(RESULT_STR)
 
 $(TARGET): 
 	$(MAKE) -C .. 
